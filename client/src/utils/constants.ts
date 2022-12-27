fb4c4605fe9ab5f02ff5c5930301264478a94e46
IvanNugiban <aa1245541@gmail.com>
2022-12-27 23:51:57 +0200

    Fixed some problems

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,7 @@
+export const baseUrl = process.env.REACT_APP_SERVER_URL;
+export const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+export const githubRepositoriesLink = "https://github.com/IvanNugiban?tab=repositories";
+export const githubLink = "https://github.com/IvanNugiban"
+export const instagramLink = "https://www.instagram.com/ivan_peshikov/"
+export const twitterLink = "https://twitter.com/IvanNugiban";
+
