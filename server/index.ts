473e0e233667ad8922e0ced66a118939cc519914
IvanNugiban <aa1245541@gmail.com>
2022-12-29 19:27:39 +0200

    Cors fix

--- server/index.ts
+++ server/index.ts
@@ -14,9 +14,7 @@ const PORT = process.env.PORT || 8080;
 const dbUrl = process.env.dbUrl || ""
 
 app.use(express.json());
-app.use(cors({
-    origin: process.env.baseUrl
-}));
+app.use(cors());
 app.use("/api/email", emailRoutes);
 app.use("/api/articles", articlesRoutes);
 app.use("/api/categories", categoriesRoutes);
